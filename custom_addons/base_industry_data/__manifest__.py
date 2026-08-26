bd4f276035c549606b37da2c40551825744f143f
Krishna Kumar Sah <work.krishnasah@gmail.com>
2026-08-26 14:57:42 +0545

    working hotel

---
+++ custom_addons/base_industry_data/__manifest__.py
@@ -0,0 +1,29 @@
+{
+    'name': 'Industry base',
+    'author': 'Odoo S.A.',
+    'version': '1.1',
+    'category': 'Hidden/Tools',
+    'depends': [
+        'base',
+        'knowledge',
+    ],
+    'data': [
+        'data/knowledge_tour.xml',
+        'data/res_partner_category.xml',
+    ],
+    'demo': [
+        'demo/ir_cron.xml',
+        'demo/res_users.xml',
+        'demo/res_partner.xml',
+        'demo/res_partner_category.xml',
+    ],
+    'license': 'OEEL-1',
+    'assets': {
+        'web.assets_backend': [
+            'base_industry_data/static/src/js/my_tour.js',
+        ],
+    },
+    'cloc_exclude': [
+        'static/src/js/my_tour.js',
+    ],
+}
