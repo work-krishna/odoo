fa02dbc76a663c9addbcc7dba986fcbab9512f3d
Krishna Kumar Sah <work.krishnasah@gmail.com>
2026-09-02 18:02:49 +0545

    copied fonepay from backup to custom_addons

---
+++ custom_addons/payment_fonepay/tests/test_payment_provider.py
@@ -0,0 +1,33 @@
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+from odoo.tests import tagged
+
+from odoo.addons.payment_fonepay.tests.common import FonepayCommon
+
+
+@tagged('post_install', '-at_install')
+class TestPaymentProvider(FonepayCommon):
+
+    def test_incompatible_with_unsupported_currencies(self):
+        """ Test that Fonepay is filtered out from compatible providers when the currency is not
+        supported. """
+        compatible_providers = self.env['payment.provider']._get_compatible_providers(
+            self.company_id, self.partner.id, self.amount, currency_id=self.env.ref('base.EUR').id
+        )
+        self.assertNotIn(self.fonepay, compatible_providers)
+
+    def test_compatible_with_npr(self):
+        """ Test that Fonepay is proposed as a compatible provider for NPR payments. """
+        compatible_providers = self.env['payment.provider']._get_compatible_providers(
+            self.company_id, self.partner.id, self.amount, currency_id=self.currency.id
+        )
+        self.assertIn(self.fonepay, compatible_providers)
+
+    def test_build_request_url_switches_on_state(self):
+        """ Test that the API host used depends on whether the provider is live or in test mode.
+        """
+        self.fonepay.state = 'test'
+        self.assertIn('dev-merchantapi', self.fonepay._build_request_url('/foo'))
+
+        self.fonepay.state = 'enabled'
+        self.assertNotIn('dev-merchantapi', self.fonepay._build_request_url('/foo'))
